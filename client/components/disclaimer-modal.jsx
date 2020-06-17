6576e2c68597b8e1655dd070f97e52d354b2d543
Mohsinul Huq <kusholhuq@gmail.com>
2020-06-17 16:19:52 -0700

    I need to change this to a stateful modal component because it contains a form, so I will style it and then add state

--- client/components/disclaimer-modal.jsx
+++ client/components/disclaimer-modal.jsx
@@ -11,11 +11,24 @@ function DisclaimerModal(props) {
   return (
     <div className={`disclaimer-modal-container ${className} justify-content-center align-items-center`}>
       <div className='pb-4 d-flex justify-content-center flex-column align-content-around room-closed-modal shadow'>
-        <div className='match-icon'>
-
-        </div>
-        <p className='text-center lead'> sorry this room has been closed</p>
-        <button type='button' onClick={props.hideDisclaimerModal} className='btn shadow-sm align-self-center'>Ok</button>
+        <h2>Welcome to METALOGY!</h2>
+        <p>Please note that this website is a content management application
+            created for the purpose of demonstration. Check the box below to acknowledge
+            that the merchandise shown here is not available for purchase, that
+            you will not provide genuine financial or personal information, and
+            that you are aware no purchase will truly be processed. </p>
+        <form onSubmit={props.hideDisclaimerModal}>
+          <div>
+            <input type="checkbox" className='form-check-input' id='acknowledgement' required/>
+          </div>
+          <div>
+            <label htmlFor="acknowledgement" className='form-check-label'>I acknowledge
+              that this is strictly a demo application.</label>
+          </div>
+          <div className='btn-group w-100 p-1'>
+            <button className='btn btn-danger w-100'>Proceed</button>
+          </div>
+        </form>
       </div>
 
     </div>
