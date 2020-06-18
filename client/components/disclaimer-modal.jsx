e5ac6fb94af020f1f4776404c2df37b4d0d8e3c0
Mohsinul Huq <kusholhuq@gmail.com>
2020-06-17 17:06:06 -0700

    modal is reasonably styled at this point, need to add state to it now

--- client/components/disclaimer-modal.jsx
+++ client/components/disclaimer-modal.jsx
@@ -9,9 +9,9 @@ function DisclaimerModal(props) {
   }
 
   return (
-    <div className={`disclaimer-modal-container ${className} justify-content-center align-items-center`}>
-      <div className='pb-4 d-flex justify-content-center flex-column align-content-around room-closed-modal shadow'>
-        <h2>Welcome to METALOGY!</h2>
+    <div className={`${className} backdrop d-flex`}>
+      <div className='modal-contents m-auto bg-white border rounded mt-5 p-3'>
+        <h2 className='text-center'>Welcome to METALOGY!</h2>
         <p>Please note that this website is a content management application
             created for the purpose of demonstration. Check the box below to acknowledge
             that the merchandise shown here is not available for purchase, that
@@ -19,10 +19,10 @@ function DisclaimerModal(props) {
             that you are aware no purchase will truly be processed. </p>
         <form onSubmit={props.hideDisclaimerModal}>
           <div>
-            <input type="checkbox" className='form-check-input' id='acknowledgement' required/>
+            <input type="checkbox" className='form-check-input ml-1' id='acknowledgement' required/>
           </div>
           <div>
-            <label htmlFor="acknowledgement" className='form-check-label'>I acknowledge
+            <label htmlFor="acknowledgement" className='form-check-label ml-4 mb-3'>I acknowledge
               that this is strictly a demo application.</label>
           </div>
           <div className='btn-group w-100 p-1'>
